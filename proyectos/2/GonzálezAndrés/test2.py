10cbd04851ed7e3651e806b96dc8b62cdf507474
Andrés González <gandresto@outlook.com>
2019-10-02 01:25:15 -0500

    Algunas pruebas

--- proyectos/2/GonzálezAndrés/test2.py
+++ proyectos/2/GonzálezAndrés/test2.py
@@ -1,12 +1,24 @@
-import restaurante as rest
+# -*- coding: utf-8 -*-
+#%%
 import restaurante_v2 as r2
 from faker import Faker
 import random
 from time import sleep
-
-
+#%%
 fake = Faker(locale='es_mx')
 r2.Cliente.obtenerTodos()
 r2.Cliente.crear(fake.name())
 r2.Cliente.crear(fake.name())
-r2.Cliente.obtenerTodos()
+r2.Cliente.crear(fake.name())
+#%%
+clientes = r2.Cliente.obtenerTodos()
+#%%
+r2.Orden.crear(idCliente = 0, idMesero = 1)
+r2.Orden.crear(idCliente = 1, idMesero = 1)
+r2.Orden.crear(idCliente = 3, idMesero = 1)
+#%%
+ordenes = r2.Orden.obtenerTodos()
+#%%
+c = r2.Cliente.obtener(0)
+#%%
+o1 = c.orden
